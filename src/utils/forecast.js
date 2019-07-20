13451e4a4a283a91102701d86d8a9c9dd25a9ef2
pablo1803 <pablolola@live.fr>
2019-07-20 21:50:20 +0200

    Add about a new data in forecast and modification of css style

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,14 +2,13 @@ const request = require('request')
 
 const forecast = (latitude , longitude , callback) => {
    const url = 'https://api.darksky.net/forecast/39c4e8060a136e0ef7b8745f24254d14/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?units=si&lang=fr'
-   request({ url: url, json: true }, (err, { body, body: { currently: { temperature, precipProbability} } } ) => {
+   request({ url: url, json: true }, (err, { body, body: { currently: { temperature, precipProbability, humidity} } } ) => {
       if(err){
          callback('Unable to connect to weather service ...')
       }else if(body.error){
          callback('Unable to find location')
       }else{
-         
-         callback(undefined, body.daily.data[0].summary + 'La temperature est a ' + temperature +'°C, il y a ' + precipProbability + '% chance de pleuvoir.')
+         callback(undefined, body.daily.data[0].summary + 'La temperature est a ' + temperature +'°C .Il y a ' + precipProbability + '% chance de pleuvoir.\nAinsi qu\'un taux d\'humiditée de ' + Math.round(humidity*100) +'% .')
       }
    })
 }
